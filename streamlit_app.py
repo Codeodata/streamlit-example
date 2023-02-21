903772d523332ec285c7e4e22c50f7b5aeaf52f7
Codeodata <114332878+Codeodata@users.noreply.github.com>
2023-02-21 20:52:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,49 +2,60 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import openpyxl
-
+from PIL import Image
+from datetime import datetime
 
 st.set_page_config(page_title='CDT')
 
 archivo_cdt = st.sidebar.file_uploader('Choose a CSV file', type='csv')
 
-option1 = st.selectbox(
-    'Participante 1:',
-    ('','Carlos Novoa', 'Gonzales Ivan', 'Pacciarioni Gastón', 'Barrionuevo Matías','Brian LLanos','Emiliano Godoy', 'Paula Aviles','Rodrigo Ginenez')
-)
+# Dividir la pantalla en dos columnas
+col1, col2, col3 = st.columns(3)
 
-option2 = st.selectbox(
-    'Participante 2:',
-    ('','Carlos Novoa', 'Gonzales Ivan', 'Pacciarioni Gastón', 'Barrionuevo Matías','Brian LLanos','Emiliano Godoy', 'Paula Aviles','Rodrigo Ginenez')
-)
-
-option4 = st.selectbox(
-    'Participante 3:',
+# Mostrar los partcipantes
+with col1:
+    option1 = st.selectbox(
+    ' Participante 1:',
     ('','Carlos Novoa', 'Gonzales Ivan', 'Pacciarioni Gastón', 'Barrionuevo Matías','Brian LLanos','Emiliano Godoy', 'Paula Aviles','Rodrigo Ginenez')
 )
 
 
-option3 = st.selectbox(
-    'Horario:',
-    ('','Turno Mañana - 8:00 a 16:00','Turno Tarde - 16:00 to 00:00','Turno Noche - 00:00 to 08:00')
+with col2:
+    option2 = st.selectbox(
+    ' Participante 2:',
+    ('','Carlos Novoa', 'Gonzales Ivan', 'Pacciarioni Gastón', 'Barrionuevo Matías','Brian LLanos','Emiliano Godoy', 'Paula Aviles','Rodrigo Ginenez')
 )
+
+
+with col3:
+    option4 = st.selectbox(
+    ' Participante 3:',
+    ('','Carlos Novoa', 'Gonzales Ivan', 'Pacciarioni Gastón', 'Barrionuevo Matías','Brian LLanos','Emiliano Godoy', 'Paula Aviles','Rodrigo Ginenez')
+)
+
 
 def add_note():
-    # Aquí puedes agregar el código para guardar la nota en una base de datos
-    st.write(f"Comentarios del Turno: {note}")
-    st.write(f"Comentarios del Turno: {note2}")
-    st.write(f"Comentarios del Turno: {note3}")
-# Mostrar el campo de entrada de texto para agregar notas
-note = st.text_input('*', key='new_note')
-note2 = st.text_input('*', key='new_note2')
-note3 = st.text_input('*', key='new_note3')
+    # Aquí puedes agregar el código para guardar la nota en una base de datos.
+    st.subheader(f"Comentarios del Turno:")
+    st.write(f"* {note}")
+    st.write(f"* {note2}")
+    st.write(f"* {note3}")
 
-from PIL import Image
+# Mostrar el campo de entrada de texto para agregar notas
+st.subheader('Comentarios del Turno👇')
+note = st.text_input('', key='new_note')
+note2 = st.text_input('', key='new_note2')
+note3 = st.text_input('', key='new_note3')
+
+
+option3 = st.sidebar.radio(
+        'Horario:',
+        ('','Turno Mañana - 8:00 a 16:00','Turno Tarde - 16:00 to 00:00','Turno Noche - 00:00 to 08:00')
+    )
+
+#Mostrar Imagen TN3
 image = Image.open('tecnotree.jpg')
 st.image(image, caption='')
-
-
-from datetime import datetime
 
 # Obtener la fecha actual
 today = datetime.today().strftime('%Y-%m-%d')
@@ -52,21 +63,11 @@ today = datetime.today().strftime('%Y-%m-%d')
 # Mostrar la fecha actual en Streamlit
 st.header('Service Desk - Cambio de Turno - {}'.format(today) )
 
-# Dividir la pantalla en dos columnas
-col1, col2, col3 = st.columns(3)
-
 # Mostrar Turnos del Equipo
 st.subheader('{}'.format(option3))
 
-
-# Mostrar los partcipantes
-with col1:
-    st.subheader('{}'.format(option1))
-with col2:
-    st.subheader('{}'.format(option2))
-with col3:
-    st.subheader('{}'.format(option4))
-
+# Mostrar participantes
+st.subheader(f"Participantes:  {option1} - {option2} - {option4}")
 
 
 add_note()
@@ -84,7 +85,7 @@ if acti_cenam:
         'Encargado:',
         ('','Carlos Novoa', 'Gonzales Ivan', 'Pacciarioni Gastón', 'Barrionuevo Matías','Brian LLanos','Emiliano Godoy', 'Paula Aviles','Rodrigo Ginenez')
         )
-        horario = st.sidebar.radio("Inicio Actividad",('','22:00','23:00','00:00','1:00','2:00'))
+        horario = st.sidebar.radio("Inicio Actividad CENAM",('','22:00','23:00','00:00','1:00','2:00'))
 
     st.subheader('Actividad CENAM:{}'.format(encargado))
     st.subheader('* La actividad en CENAM comienza a las : {}hs'.format(horario))
@@ -95,7 +96,7 @@ if acti_aup:
         'Encargado 2:',
         ('','Carlos Novoa', 'Gonzales Ivan', 'Pacciarioni Gastón', 'Barrionuevo Matías','Brian LLanos','Emiliano Godoy', 'Paula Aviles','Rodrigo Ginenez')
         )
-        horario2 = st.sidebar.radio("Inicio 2da Actividad",('','22:00','23:00','00:00','1:00','2:00'))
+        horario2 = st.sidebar.radio("Inicio Actividad AUP",('','22:00','23:00','00:00','1:00','2:00'))
 
     st.subheader('---')
     st.subheader('Actividad AUP:{}'.format(encargado2))
@@ -107,7 +108,7 @@ if acti_ecuador:
         'Encargado 3:',
         ('','Carlos Novoa', 'Gonzales Ivan', 'Pacciarioni Gastón', 'Barrionuevo Matías','Brian LLanos','Emiliano Godoy', 'Paula Aviles','Rodrigo Ginenez')
         )
-        horario3 = st.sidebar.radio("Inicio 3ra Actividad",('','22:00','23:00','00:00','1:00','2:00'))
+        horario3 = st.sidebar.radio("Inicio Actividad ECUADOR",('','22:00','23:00','00:00','1:00','2:00'))
     
     st.subheader('Actividad ECUADOR:{}'.format(encargado3))
     st.subheader('La actividad en ECUADOR comienza a las : {}hs'.format(horario3))
@@ -120,4 +121,3 @@ if archivo_cdt:
     # Convertir el DataFrame a una tabla HTML
     tabla_html = df.to_html(index=False)
     st.write(tabla_html, unsafe_allow_html=True)
-
