93e67dd3976999007db2bd9146250181b66c61e6
Codeodata <114332878+Codeodata@users.noreply.github.com>
2023-03-02 05:02:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,7 +94,7 @@ with col1:
  #Imagen    
 with col2:
    st_lottie_animation = load_lottiefile("coding.json")
-    st_lottie(st_lottie_animation,
+   st_lottie(st_lottie_animation,
               speed=0.9,
               reverse=False,
               loop=True,
