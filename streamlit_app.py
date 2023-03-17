8527bfa6b5499198a0a45dd5c9f5e594b4c2fb7a
Codeodata <114332878+Codeodata@users.noreply.github.com>
2023-03-17 07:01:25 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,61 +13,34 @@ st.set_page_config(layout="wide", page_title="CDT")
 def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
-
 #para importar imagen con url
 def load_lottieurl(url: str):
     r = requests.get(url)
     if r.status_code != 200:
         return None
     return r.json()
-
 st.write("<h1 style='font-size:60px'>Cambio de Turno - Service Desk</h1>", unsafe_allow_html=True)
-
 # Dividir la pantalla en dos columnas
 col1, col2, col3 = st.columns(3)
-
-# SIDEBAR
-#Lector de archivo CSV
-archivo_cdt = st.sidebar.file_uploader('Choose a CSV file', type='csv')
-
-# Define the radio button for selecting the turno
-#st.sidebar.subheader('Seleccione su Turno⌚')
-#options = st.sidebar.radio('', options=['Turno Mañana', 'Turno Tarde','Turno Noche'])
-
-# Use the selected option to set the corresponding value of horario
-#if options == 'Turno Mañana':
-#    hora = '8:00 a 16:00hs'
-#elif options == 'Turno Tarde':
-#    hora = '16:00 a 00:00hs'
-#elif options == 'Turno Noche':
-#    hora = '00:00 a 8:00hs'
-    
-# Actividades
-st.sidebar.subheader('Actividades Service Desk 💻')
-acti_cenam = st.sidebar.checkbox('Actividad Cenam')
-acti_aup = st.sidebar.checkbox('Actividad AUP')
-acti_ecuador = st.sidebar.checkbox('Actividad Ecuador')
-
-# PANTALLA PRINCIPAL
 # Mostrar los participantes
 with col1:
     option1 = st.selectbox(
     ' Participante 1 🧑‍💻',
     ('','Novoa Carlos', 'Gonzales Ivan', 'Pacciarioni Gastón', 'Barrionuevo Matías','LLanos Brian','Godoy Emiliano','Fernandez Diego' ,'Aviles Paula','Gimenez Rodrigo')
-    )
-
+)
 with col2:
     option2 = st.selectbox(
     ' Participante 2 👩‍💻',
     ('','Novoa Carlos', 'Gonzales Ivan', 'Pacciarioni Gastón', 'Barrionuevo Matías','LLanos Brian','Godoy Emiliano','Fernandez Diego' ,'Aviles Paula','Gimenez Rodrigo')
     )
-
 with col3:
     option4 = st.selectbox(
     ' Participante 3 🧑‍💻',
     ('','Novoa Carlos', 'Gonzales Ivan', 'Pacciarioni Gastón', 'Barrionuevo Matías','LLanos Brian','Godoy Emiliano','Fernandez Diego' ,'Aviles Paula','Gimenez Rodrigo')
     )
-
+    
+#Lector de Archivo Excel
+archivo_cdt = st.sidebar.file_uploader('Choose a CSV file', type='csv')
 # Comentarios    
 def add_note():
  # Aquí puedes agregar el código para guardar la nota en una base de datos.
@@ -79,14 +52,22 @@ def add_note():
     st.write(f" {note4}")    
  #   st.subheader('-')
  #  st.write(f" {note3}")
-
 #if not acti_aup or acti_cenam or acti_ecuador:
  #   st.subheader(f"🗓️Comentarios de la Actividad:")
  #   st.write(f" {note4}")
   #  st.write(f" {note5}")
   #  st.subheader('-')
   #  st.write(f" {note6}")
-    
+# Mostrar el campo de entrada de texto para agregar notas
+with col2:
+    st.subheader('Comentarios  del Turno  ')
+    note = st.text_input('Comentario 1', key='new_note')
+    note2 = st.text_input('Comentario 2', key='new_note2')
+with col3:
+    st.subheader('✏️🗒️')
+    note3 = st.text_input('Comentario 3', key='new_note3')
+    note4 = st.text_input('Comentario 4', key='new_note4')
+  
 # Imagen    
 with col1:
     st_lottie_animation2 = load_lottieurl("https://assets10.lottiefiles.com/packages/lf20_m9zragkd.json")
@@ -98,17 +79,6 @@ with col1:
               height=None,
               width=None,
               key=None) 
-# Mostrar el campo de entrada de texto para agregar notas
-with col2:
-    st.subheader('Comentarios  del Turno  ')
-    note = st.text_input('Comentario 1', key='new_note')
-    note2 = st.text_input('Comentario 2', key='new_note2')
-with col3:
-    st.subheader('✏️🗒️')
-    note3 = st.text_input('Comentario 3', key='new_note3')
-    note4 = st.text_input('Comentario 4', key='new_note4')
-  
-
   #  st.subheader('-----------------------------------------------')
  #Imagen    
 #with col2:
@@ -130,27 +100,28 @@ with col3:
    #     note5 = st.text_input('', key='new_note5')
        #note6 = st.text_input('', key='new_note6')
     
-
-
+# Turnos
+option3 = st.sidebar.radio(
+        'Horario ⌚',
+        ('','8:00 a 16:00','16:00 a 00:00','00:00 a 08:00')
+        )
 #Mostrar Imagen TN3
 st.subheader('-')
 st.subheader('-')
 image = Image.open('tn3.jpg')
 st.image(image, caption='',use_column_width=False)
-
 # Obtener la fecha actual
 today = datetime.today().strftime('%d/%m/%y')
-
 # Mostrar la fecha actual en Streamlit
-
-# st.title(f'Service Desk - Cambio de Turno - {today} - {hora}')
-
+st.title(f'Service Desk - Cambio de Turno - {today} - {option3}')
 # Mostrar Participantes
 # st.subheader('-')
 st.subheader(f"🧑‍💻Participantes: {option1} - {option2} - {option4}")
-
 add_note()
-
+st.sidebar.subheader('Actividades Service Desk 💻')
+acti_cenam = st.sidebar.checkbox('Actividad Cenam')
+acti_aup = st.sidebar.checkbox('Actividad AUP')
+acti_ecuador = st.sidebar.checkbox('Actividad Ecuador')
 if acti_cenam:
     with col1:
         encargado = st.sidebar.selectbox(
@@ -158,7 +129,6 @@ if acti_cenam:
         ('-','Novoa Carlos', 'Gonzales Ivan', 'Pacciarioni Gastón', 'Barrionuevo Matías','LLanos Brian','Godoy Emiliano','Fernandez Diego' ,'Aviles Paula','Gimenez Rodrigo')
         )
         horario = st.sidebar.radio("Inicio Actividad CENAM",('','22:00','23:00','00:00','1:00','2:00'))
-
     st.subheader(f'➡️Actividad CENAM: {encargado} - Comienza a las {horario}hs')
                  
 if acti_aup:
@@ -168,10 +138,8 @@ if acti_aup:
         ('-','Novoa Carlos', 'Gonzales Ivan', 'Pacciarioni Gastón', 'Barrionuevo Matías','LLanos Brian','Godoy Emiliano','Fernandez Diego' ,'Aviles Paula','Gimenez Rodrigo')
         )
         horario2 = st.sidebar.radio("Inicio Actividad AUP",('','22:00','23:00','00:00','1:00','2:00'))
-
     st.subheader(f'➡️Actividad AUP: {encargado2} - Comienza a las {horario2}hs')
     
-
 if acti_ecuador:
     with col1:
         encargado3 = st.sidebar.selectbox(
@@ -182,33 +150,17 @@ if acti_ecuador:
     
     st.subheader(f'➡️Actividad ECUADOR: {encargado3} - Comienza a las {horario3} hs')
     
-
 st.subheader('-')
-
 # Leer el excel
 if archivo_cdt:
     df = pd.read_csv(archivo_cdt, engine='python')
-    #Tarjeta de Total de Casos
     num_filas = df.shape[0]
-    if num_filas == 0:
-        st.subheader('No se reportaron nuevos casos en este turno')
-    else:
-        st.subheader(f'Total de Casos: {num_filas}')
-        # Convertir el DataFrame a una tabla HTML
-        tabla_html = df.to_html(index=False)
-        st.write(tabla_html, unsafe_allow_html=True)
-    
+    #num_filas = df.shape[0]
     #Tarjeta de casos Asignados:
     #asignado = (df['Estado'] == 'Asignado').sum()
     #st.subheader(f'Casos en Asignados: {asignado}')
-    #Tarjeta de casos Cerrados:
-    #cerrado = (df['Estado'] == 'Closed').sum()
-    #st.subheader(f'Casos Solucionados: {cerrado}')
-    #Tarjeta de casos en Investigacion:
-    #investigacion = (df['Estado'] == 'En investigacion').sum()
-    #st.subheader(f'Casos en Investigación: {investigacion}')
-
-
-    
-    
-    
+    st.subheader(f'Total de Casos: {num_filas}')    
+    #st.subheader(f'Total de Casos: {num_filas}')    
+    # Convertir el DataFrame a una tabla HTML
+    tabla_html = df.to_html(index=False)
+    st.write(tabla_html, unsafe_allow_html=True)
