9fca59d75fe0aea5715fea8cb2b1e8e6d548bbb2
Codeodata <114332878+Codeodata@users.noreply.github.com>
2023-03-17 03:18:36 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ col1, col2, col3 = st.columns(3)
 archivo_cdt = st.sidebar.file_uploader('Choose a CSV file', type='csv')
 
 # Define the radio button for selecting the turno
-turno = st.sidebar.subheader('Seleccione su Turno⌚')
-options = st.sidebar.radio(turno, options=['Turno Mañana', 'Turno Tarde','Turno Noche'])
+st.sidebar.subheader('Seleccione su Turno⌚')
+options = st.sidebar.radio('', options=['Turno Mañana', 'Turno Tarde','Turno Noche'])
 
 # Use the selected option to set the corresponding value of horario
 if options == 'Turno Mañana':
