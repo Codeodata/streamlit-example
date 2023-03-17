4369f6fda6349d152adb63bf4cc481c99ea47af1
Codeodata <114332878+Codeodata@users.noreply.github.com>
2023-03-17 02:28:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ col1, col2, col3 = st.columns(3)
 #Lector de archivo CSV
 archivo_cdt = st.sidebar.file_uploader('Choose a CSV file', type='csv')
 # Horarios
-st.sidebar.radio('Seleccione su Turno⌚')
+st.sidebar.subheader('Seleccione su Turno⌚')
 st.radio('Seleccione una opción:', options=['Turno Mañana', 'Turno Tarde','Turno Noche'], index=0) 
 if options=='Turno Mañana':
     horario = '8:00 a 16:00hs'
